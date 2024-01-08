273614a0e44fcd561a673e329171381733d91183
MZS01-KYOUNGH <mzs01-kyoungh@MZS01-KYOUNGH.local>
2024-01-08 10:32:14 +0900

    dispatchQueue

--- step1/RxSwift+MVVM/ViewController.swift
+++ step1/RxSwift+MVVM/ViewController.swift
@@ -31,20 +31,28 @@ class ViewController: UIViewController {
             self?.view.layoutIfNeeded()
         })
     }
-
+    
+    func downloadJSon(_ url: String, _ completion: @escaping (String?) -> Void){
+        DispatchQueue.global().async {
+            let url = URL(string: url)!
+            let data = try! Data(contentsOf: url)
+            let json = String(data: data, encoding: .utf8)
+            DispatchQueue.main.async {
+                completion(json)
+            }
+        }
+    }
     // MARK: SYNC
 
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
 
     @IBAction func onLoad() {
         editView.text = ""
-        setVisibleWithAnimation(activityIndicator, true)
-
-        let url = URL(string: MEMBER_LIST_URL)!
-        let data = try! Data(contentsOf: url)
-        let json = String(data: data, encoding: .utf8)
-        self.editView.text = json
+        self.setVisibleWithAnimation(self.activityIndicator, true)
         
-        self.setVisibleWithAnimation(self.activityIndicator, false)
+        downloadJSon(MEMBER_LIST_URL) { json in
+            self.editView.text = json
+            self.setVisibleWithAnimation(self.activityIndicator, false)
+        }
     }
 }
